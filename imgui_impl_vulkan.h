766ce72f6ac60e6053e4183085c8d1278474c334
sigmoid <adam.swaggoner@gmail.com>
2021-09-12 22:05:32 -0500

    Bringing imgui up to date

--- imgui_impl_vulkan.h
+++ imgui_impl_vulkan.h
@@ -51,7 +51,6 @@ IMGUI_IMPL_API void     ImGui_ImplVulkan_DestroyFontUploadObjects();
 IMGUI_IMPL_API void     ImGui_ImplVulkan_SetMinImageCount(uint32_t min_image_count); // To override MinImageCount after initialization (e.g. if swap chain is recreated)
 IMGUI_IMPL_API ImTextureID    ImGui_ImplVulkan_AddTexture(VkSampler sampler, VkImageView image_view, VkImageLayout image_layout);
 
-
 //-------------------------------------------------------------------------
 // Internal / Miscellaneous Vulkan Helpers
 // (Used by example's main.cpp. Used by multi-viewport features. PROBABLY NOT used by your own engine/app.)
